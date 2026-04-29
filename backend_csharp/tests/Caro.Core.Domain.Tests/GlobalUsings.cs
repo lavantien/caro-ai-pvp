437163c0676f9adf93237a2d0213e397252bceeb
lavantien <lavantien@users.noreply.github.com>
2026-04-29 12:13:47 +0700

    chore: remove C# backend, Go port complete

--- backend_csharp/tests/Caro.Core.Domain.Tests/GlobalUsings.cs
+++
@@ -1,7 +0,0 @@
-global using Xunit;
-global using FluentAssertions;
-global using System.Linq;
-global using Caro.Core.Domain.Entities;
-global using Caro.Core.Domain.ValueObjects;
-global using Caro.Core.Domain.Interfaces;
-global using Caro.Core.Domain.Exceptions;
